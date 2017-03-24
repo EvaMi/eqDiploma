a23e520c57b124a9175f2e7d6bbbe99551dd3310
EvaMi <milova_evg@mail.ru>
2017-03-25 02:12:00 +0300

    Sending without nodes. TODO: Add nodes to diff. ports; change size of buffers and sequense format; send all arrays.

--- rec_data_node.cpp
+++ rec_data_node.cpp
@@ -1,55 +1,90 @@
-//seq-распределение
-#include <seq/seq.h>
-//gl.h-Includes OpenGL and GLEW headers. EQ_GL_CALL(code)
-#include <eq/gl.h>
-#include <co/co.h>
-#include <lunchbox/file.h>
+#include <lunchbox/test.h>
+#include <co/buffer.h>
+#include <co/connection.h>
+#include <co/connectionDescription.h>
+#include <co/connectionSet.h>
+#include <co/init.h>
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <cstdlib> // Для работы с функцией system()
-
-#include <eqHello/fragmentShader.glsl.h>
-#include <eqHello/vertexShader.glsl.h>
+#include <lunchbox/clock.h>
+#include <lunchbox/monitor.h>
+#include <iostream>
 
 #define PACKETSIZE (123456)
+#define RUNTIME (1000) // ms
 
-namespace data_node
+lunchbox::Monitor< bool > s_done( false );
+
+namespace
 {
+class Reader : public lunchbox::Thread
+{
+public:
+    explicit Reader( co::ConnectionPtr connection ) : connection_( connection )
+        { TEST( start( )); }
 
-  class Reader : public lunchbox::Thread
-  {
-  public:
-    explicit Reader( co::ConnectionPtr connection ): connection_( connection ){}
-      void run() override
-      {
-          co::ConnectionPtr listener = connection_;
-          connection_ = listener->acceptSync();
+    void run() override
+    {
+        co::ConnectionPtr listener = connection_;
+        connection_ = listener->acceptSync();
+        TESTINFO( connection_, listener->getDescription( ));
 
-          co::Buffer buffer;
-          co::BufferPtr syncBuffer;
-          buffer.reserve( PACKETSIZE );
-          uint64_t& sequence = *reinterpret_cast< uint64_t* >( buffer.getData( ));
-          sequence = 0;
-          //uint64_t i = 0;
+        co::Buffer buffer;
+        co::BufferPtr syncBuffer;
+        buffer.reserve(  sizeof(float)*2 );
+        float& sequence = *reinterpret_cast< float* >( buffer.getData( ));
+        sequence = 0;
+        //uint64_t i = 0;
 
+        s_done = false;
 
-          while( sequence != 0xdeadbeef )
-          {
-              connection_->recvNB( &buffer, 123456 );
-              sequence = 0xdeadbeef;
-              buffer.setSize( 0 );
-          }
+        while( sequence != 1010101010 )
+        {
+            connection_->recvNB( &buffer,  sizeof(float) );
+            TEST( connection_->recvSync( syncBuffer ));
+            TEST( syncBuffer == &buffer );
+            TEST( buffer.getSize() ==  sizeof(float) );
+            std::cout<<  "Test buffer!!!"  << std::endl;
+            std::cout<<  sequence  << std::endl;
+            //TEST( sequence == ++i || sequence == 0xdeadbeef );
+            buffer.setSize( 0 );
+        }
 
-          connection_->recvNB( &buffer, 123456 );
-          connection_ = 0;
-          std::cout<<"buffer" << std::endl;
-          std::cout<<buffer.getData( ) << std::endl;
-      }
-    private:
-      co::ConnectionPtr connection_;
-  };
+        s_done = true;
+        connection_->recvNB( &buffer,  sizeof(float) );
+        TEST( !connection_->recvSync( syncBuffer ));
+#ifndef _WIN32
+        TEST( connection_->isClosed( ));
+#endif
+        connection_ = 0;
+    }
 
+private:
+    co::ConnectionPtr connection_;
+};
+bool _initialize( co::ConnectionDescriptionPtr desc,
+                  co::ConnectionPtr& listener,
+                  co::ConnectionPtr& writer )
+{
+    desc->setHostname( "127.0.0.1" );
+    desc->port=49868;
+
+    listener = co::Connection::create( desc );
+    if( !listener )
+    {
+        std::cout << ": not supported" << std::endl;
+        return false;
+    }
+    const bool listening = listener->listen();
+    TESTINFO( listening, desc );
+    listener->acceptNB();
+    desc->setHostname( "127.0.0.1" );
+    //desc->port=52822;
+    //co::LocalNodePtr server = new co::LocalNode;
+
+    writer = co::Connection::create( desc );
+    TEST( writer );
+    return true;
+}
 typedef lunchbox::RefPtr<co::LocalNode> LocalNodePtr;
 typedef lunchbox::RefPtr<co::Node> NodePtr;
 typedef lunchbox::RefPtr<co::Buffer> BufferPtr;
@@ -57,83 +92,51 @@ typedef lunchbox::RefPtr<co::Connection>ConnectionPtr;
 typedef lunchbox::RefPtr<co::ConnectionDescription>ConnectionDescriptionPtr;
 }
 
-
-int main(const int argc, char** argv)
+int main( int argc, char **argv )
 {
-  //pointers
-  co::init(argc, argv);
-  data_node::ConnectionDescriptionPtr connDesc=new co::ConnectionDescription;
-  co::ConnectionPtr con1;
+    TEST(( PACKETSIZE % 8 ) == 0 );
+    co::init( argc, argv );
 
-  connDesc = new co::ConnectionDescription;
-  connDesc->type = co::CONNECTIONTYPE_TCPIP;
-  connDesc->setHostname( "127.0.0.1" );
-  connDesc->setFilename("cfiles_");
-  connDesc->port=49868;
+    co::ConnectionDescriptionPtr desc = new co::ConnectionDescription;
+    desc->type = co::CONNECTIONTYPE_TCPIP;;
 
-  data_node::LocalNodePtr client = new co::LocalNode;
-  client->addConnectionDescription( connDesc );
-  //client->listen( );
-  data_node::ConnectionPtr listener;
-  listener = co::Connection::create( connDesc );
-  listener->listen();
-  listener->acceptNB();
+    co::ConnectionPtr writer;
+    co::ConnectionPtr listener;
+    if( !_initialize( desc, listener, writer ))
+          std::cout<< "Dont inicialize" << std::endl;
 
-  //Local node for proxy
-  data_node::LocalNodePtr server = new co::LocalNode;
-  connDesc = new co::ConnectionDescription;
-  connDesc->setHostname( "127.0.0.1" );
-  connDesc->port=52822;
-  connDesc->setFilename("sfiles_");
-  server->addConnectionDescription( connDesc );
-  server->listen( );
+    Reader readThread( listener );
+    TEST( writer->connect( ));
 
-  data_node::NodePtr serverProxy = new co::Node;
-  serverProxy->addConnectionDescription( connDesc );
-  std::cout<< "1111gldehndkhnd:" << std::endl;
-  //if(client->connect( serverProxy ))
-    //std::cout<< "Succesfull connected" << std::endl;
+    int imax=48;
+    float x0=-1;
+    float dx=0.04;
+    float x[49];
 
-    data_node::ConnectionPtr writer;
-    writer = co::Connection::create( connDesc );
+    for (int i=0; i<=imax; i++){
+             x[i]=x0+i*dx;
+    }
+    std::cout<<  "Test send!!!"  << std::endl;
+    std::cout<<  x[1]  << std::endl;
+    std::cout<<  "Test send!!!"  << std::endl;
+    float out[1];
+    //lunchbox::Clock clock;
+    //uint64_t sequence = 0;
 
-  data_node::Reader readThread( listener );
-  writer->connect( );
+    //while( clock.getTime64() < RUNTIME )
+    //{
+      //out[0] = ++sequence;
+      TEST( writer->send( &x[1], sizeof(float) ));
+    //}
 
-  //connection=serverProxy->co::Node::getConnection();
-  //std::cout<<connection->co::Connection::getDescription()<< std::endl;
-  float x0=-1;
-  float x[100];
-  float dx=0.04;
-  int imax=48;
-  for (int i=0; i<=imax; i++){
-           x[i]=x0+i*dx;
-  }
-  //uint64_t sequence = 1;
-  //++sequence;
-  //for (int i=0; i<=imax; i++){
-  //        writer->send( &x[i], sizeof(float));
-  //}
-  if(writer->send( &x, sizeof(float)*100))
-    std::cout<< "aloe:" << std::endl;
+    out[0] = 1010101010;
+    TEST( writer->send( out,  sizeof(float) ));
 
-  uint64_t sequence = 0;
-  sequence= 0xdeadbeef;
-  if(writer->send( &sequence, sizeof( uint64_t )))
-      std::cout<< "aloe:" << std::endl;
+    s_done.waitEQ( true );
+    writer->close();
+    readThread.join();
+    listener->close();
 
-  std::cout<< "gldkjhdljhd:" << std::endl;
-  std::cout<< "End:" << std::endl;
-
-  //client->disconnect( serverProxy);
-  client->close( );
-  server->close( );
-
-  serverProxy = 0;
-  client = 0;
-  server=0;
-
-  co::exit();
-  return EXIT_SUCCESS;
-
+    co::exit();
+    return EXIT_SUCCESS;
 }
