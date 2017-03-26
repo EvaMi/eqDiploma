569e27fae40e63684cdf4dc5f8d7e7a29de32796
EvaMi <milova_evg@mail.ru>
2017-03-26 04:06:10 +0300

    Sending without nodes. Send common array with x,y,z. TODO: add nodes; decide something about arrays size sending; find max buffer size(very important); create optimal array structure

--- rec_data_node.cpp
+++ rec_data_node.cpp
@@ -30,28 +30,33 @@ public:
 
         co::Buffer buffer;
         co::BufferPtr syncBuffer;
-        buffer.reserve(  sizeof(float)*2 );
-        float& sequence = *reinterpret_cast< float* >( buffer.getData( ));
-        sequence = 0;
+        buffer.reserve(sizeof(float)*23*sizeof(float)*20);
+        std::cout<<  "Max buffer size="  << std::endl;
+        std::cout<<  buffer.getMaxSize	()  << std::endl;
+        float (&sequence)[23][20] = *reinterpret_cast< float(*)[23][20] >( buffer.getData( ));
+        //sequence = 0;
         //uint64_t i = 0;
 
         s_done = false;
 
-        while( sequence != 1010101010 )
-        {
-            connection_->recvNB( &buffer,  sizeof(float) );
-            TEST( connection_->recvSync( syncBuffer ));
-            TEST( syncBuffer == &buffer );
-            TEST( buffer.getSize() ==  sizeof(float) );
-            std::cout<<  "Test buffer!!!"  << std::endl;
-            std::cout<<  sequence  << std::endl;
-            //TEST( sequence == ++i || sequence == 0xdeadbeef );
-            buffer.setSize( 0 );
-        }
+        connection_->recvNB( &buffer,  sizeof(float)*23*sizeof(float)*20);
+        TEST( connection_->recvSync( syncBuffer ));
+        TEST( syncBuffer == &buffer );
+        TEST( buffer.getSize() ==  sizeof(float)*23*sizeof(float)*20);
+        std::cout<<  "Test buffer!!!"  << std::endl;
+        std::cout<<  sequence[3][1]<< std::endl;
+        //TEST( sequence == ++i || sequence == 0xdeadbeef );
+        buffer.setSize( 0 );
+        std::cout<<  "Test buffer2!!!"  << std::endl;
 
         s_done = true;
-        connection_->recvNB( &buffer,  sizeof(float) );
+        std::cout<<  "Test buffer3!!!"  << std::endl;
+        connection_->recvNB( &buffer,  sizeof(float)*23*sizeof(float)*20);
+        std::cout<<  "Test buffer4!!!"  << std::endl;
         TEST( !connection_->recvSync( syncBuffer ));
+        std::cout<<  "Test buffer5!!!"  << std::endl;
+        //float* xm[49];
+        //xm=*syncBuffer;
 #ifndef _WIN32
         TEST( connection_->isClosed( ));
 #endif
@@ -108,29 +113,61 @@ int main( int argc, char **argv )
     Reader readThread( listener );
     TEST( writer->connect( ));
 
-    int imax=48;
+    //размеры сетки
+    int imax=20;
+    int jmax=20;
+    //размер индексного массива
+    //int sizeindex;
+    //начальная координата x
     float x0=-1;
+    //начальная координата z
+    float z0=-1;
+    //шаг сетки по оси x
     float dx=0.04;
-    float x[49];
+    //шаг сетки по оси z
+    float dz=0.04;
+    // массив для хранения координаты x
+    //float x[49];
+    // массив для хранения координаты y
+    float y[23][20];//imax+3!!!
+    //массив для хранения координаты z
+    //float z[49];
+    //массив для хранения всех координат вершин для записи в буфер
+    //float common[51][49];
+    y[0][0]=48;
+    y[0][1]=48;
 
+    //заполним массивы x и z координатами сетки
     for (int i=0; i<=imax; i++){
-             x[i]=x0+i*dx;
+             y[1][i]=x0+i*dx;
     }
+    for (int j=0; j<=jmax; j++){
+             y[2][j]=z0+j*dz;
+    }
+    // заполним массив Y значениями функции
+    for (int i=3; i<=imax; i++){
+             for (int j=0; j<=imax; j++){
+                      y[i][j]=exp(-4*(y[1][j]*y[2][j]+y[2][j]*y[2][j]));
+                      //y[j][i]=cos(0.005*time+5*(z[j]+x[i]));
+             }
+    }
+    //x[imax+1]=1010101010;
     std::cout<<  "Test send!!!"  << std::endl;
-    std::cout<<  x[1]  << std::endl;
+    std::cout<<  y[3][1] << std::endl;
+    //std::cout<<  &y << std::endl;
     std::cout<<  "Test send!!!"  << std::endl;
-    float out[1];
+    //float out[1];
     //lunchbox::Clock clock;
     //uint64_t sequence = 0;
 
     //while( clock.getTime64() < RUNTIME )
     //{
       //out[0] = ++sequence;
-      TEST( writer->send( &x[1], sizeof(float) ));
+    TEST( writer->send( &y, (sizeof(float)*23*sizeof(float)*20)));
     //}
 
-    out[0] = 1010101010;
-    TEST( writer->send( out,  sizeof(float) ));
+    //out[0] = 1010101010;
+    //TEST( writer->send( out,  sizeof(float) ));
 
     s_done.waitEQ( true );
     writer->close();
